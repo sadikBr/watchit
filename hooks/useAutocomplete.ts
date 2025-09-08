d072efc40f4a8fc5f440d445b6ccceca60f68565
SADIK Brahim <sadbrah@gmail.com>
2025-09-08 23:34:46 +0100

    implement search with autocomplete

--- hooks/useAutocomplete.ts
+++ hooks/useAutocomplete.ts
@@ -1,3 +1,61 @@
-export default function useAutocomplete() {
-  return "Autocomplete";
+import { useEffect, useRef, useState } from "preact/hooks";
+
+const DEBOUNCE_DELAY = 250;
+
+export default function useAutocomplete(query: string) {
+  const [autocompleteSearch, setAutocompleteSearch] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const timeoutId = useRef<number | null>();
+
+  async function fetchAutocompleteSuggestions(query: string) {
+    setLoading(true);
+    setError("");
+
+    try {
+      const response = await fetch(
+        `https://www.reddit.com/search.json?q=${query}&type=sr&limit=10&include_over_18=true`,
+      );
+
+      if (!response.ok) {
+        return setError(
+          `Something went wrong while trying to fetch suggestions for the query ${query}`,
+        );
+      }
+
+      const data = await response.json();
+      const children = data.data?.children;
+
+      if (!children) {
+        setAutocompleteSearch([]);
+      }
+
+      setAutocompleteSearch(children);
+    } catch {
+      setError("Something went wrong, please try again later");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (query.length < 3) return;
+
+    if (timeoutId.current) clearTimeout(timeoutId.current);
+
+    timeoutId.current = setTimeout(() => {
+      fetchAutocompleteSuggestions(query);
+    }, DEBOUNCE_DELAY);
+
+    return () => {
+      clearTimeout(timeoutId.current!);
+    };
+  }, [query]);
+
+  return {
+    autocompleteSearch,
+    loading,
+    error,
+  };
 }
--- islands/autocomplete-search.tsx
+++ islands/autocomplete-search.tsx
@@ -1,9 +1,95 @@
+import { useMemo, useState } from "preact/hooks";
+import useAutocomplete from "../hooks/useAutocomplete.ts";
+
 export default function AutocompleteSearch() {
+  const [query, setQuery] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+  const { autocompleteSearch, loading, error } = useAutocomplete(query);
+
+  const requiredRenderData = useMemo(() => {
+    return autocompleteSearch.map((item) => {
+      const data = item.data;
+
+      return {
+        display_name: data.display_name,
+        title: data.title,
+        bannerImgUrl: data.banner_background_image.split("?")[0] ||
+          data.banner_img || data.header_img || data.mobile_banner_image,
+        iconImg: data.icon_img || data.community_icon.split("?")[0],
+        description: data.public_description,
+      };
+    });
+  }, [autocompleteSearch]);
+
   return (
-    <input
-      type="text"
-      placeholder="Search ..."
-      class="flex-1 border border-orange-300 px-2 py-1 rounded-sm"
-    />
+    <div class="flex-1">
+      {isFocused && requiredRenderData.length > 0 && (
+        <div
+          class="absolute z-49 top-10 left-0 w-screen h-screen"
+          onClick={() => setIsFocused(false)}
+        />
+      )}
+      <div class="relative w-full">
+        <input
+          type="text"
+          value={query}
+          onInput={(event) => setQuery(event.currentTarget.value)}
+          onFocus={() => setIsFocused(true)}
+          placeholder="Search ..."
+          class="w-full border border-orange-300 px-2 py-1 rounded-sm"
+        />
+
+        {isFocused && query && (
+          <div class="absolute z-50 text-white left-0 top-[105%] w-full bg-blue-950">
+            <a
+              href={`http://localhost:5173/search?query=${query}`}
+              class="block px-2 py-6 bg-blue-950 hover:bg-blue-800 transition"
+            >
+              Search for: {query}
+            </a>
+
+            <div class="flex flex-col gap-1 w-full">
+              {loading
+                ? (
+                  <div class="px-2 py-4 bg-blue-950">
+                    <div class="flex items-center gap-2">
+                      <div class="w-16 aspect-square rounded-full bg-white shrink-0" />
+                      <div class="flex flex-col w-full gap-1">
+                        <div class="w-[40%] h-8 bg-gray-300" />
+                        <div class="w-[80%] h-12 bg-gray-300" />
+                      </div>
+                    </div>
+                  </div>
+                )
+                : error.length > 0
+                ? <div class="px-2 py-4 bg-blue-950">{error}</div>
+                : requiredRenderData.length > 0 && !loading &&
+                  requiredRenderData.map((item) => (
+                    <a
+                      class="block px-2 py-4 bg-blue-950 hover:bg-blue-800 transition"
+                      href={`http://localhost:5173/r/${item.display_name}`}
+                    >
+                      <div class="flex items-center gap-2">
+                        <img
+                          src={item.iconImg}
+                          alt="icon"
+                          class="w-16 aspect-square rounded-full bg-white shrink-0"
+                        />
+                        <div class="flex flex-col gap-1">
+                          <h1 class="text-md font-semibold text-orange-300">
+                            {item.display_name} - {item.title}
+                          </h1>
+                          <p class="text-sm text-gray-300">
+                            {item.description}
+                          </p>
+                        </div>
+                      </div>
+                    </a>
+                  ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
